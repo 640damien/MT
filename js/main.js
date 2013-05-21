a33a67df54d631bcbe41f0af8b2e2518fa86c003
Damien640 <640damien@gmail.com>
2013-05-21 13:15:50 +0200

    test tap

--- js/main.js
+++ js/main.js
@@ -1,16 +1,43 @@
-$.ajaxSetup({
+/*$.ajaxSetup({
      timeout: 3000
-  });
+  });*/
   
-$(document).ready(function() {
+
+var homePage = document.getElementById("homePage"),
+    page1 = document.getElementById("p1"),
+    page2 = document.getElementById("p2"),
+    currentPage = homePage;
+
+    function slidePageFrom(page, from) {
+        // Position the page at the starting position of the animation
+        page.className = "page " + from;
+        // Position the new page and the current page at the ending position of their animation with a transition class indicating the duration of the animation
+        page.className ="page transition center";
+        currentPage.className = "page transition " + (from === "left" ? "right" : "left");
+        currentPage = page;
+    }
+
+
+
+Zepto(function($){
+
+//Gestion pagination
+$("#about").click(function(){
+  slidePageFrom(page1, 'right');
+})
+
+$("#other").tap(function(){
+  slidePageFrom(page2, 'left');
+})
 
 var lon=-0.64129;
 var lat=44.810108;
 
+
 if(screen.width > 1000){
 
 //http://api.openweathermap.org/data/2.5/weather?callback=?id=2987805&units=metric
-	/*	$.getJSON("http://api.openweathermap.org/data/2.5/weather?callback=?&units=metric",{lon:lon,lat:lat},function(data){
+		/*$.getJSON("http://api.openweathermap.org/data/2.5/weather?callback=?&units=metric",{lon:lon,lat:lat},function(data){
 			$("#minlocal").append(data.main.temp_min+"~");
 			$("#maxlocal").append(data.main.temp_max);
 			$("#namelocal").html(data.name);
@@ -68,4 +95,5 @@ else {
 
 
 }
+
 });
