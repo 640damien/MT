00fe7ee59af0805a0ececc076429cc0cf17f9398
Damien640 <640damien@gmail.com>
2013-06-10 22:54:22 +0200

    ok

--- js/main.js
+++ js/main.js
@@ -26,7 +26,7 @@ var go=1;
         else if (currentPage==p1){
           $("#glo").css('height','2646px')
         }
-        $(window).scrollTop(0);
+        //$(window).scrollTop(0);
     }
 
    function getIcon(wid,cloud) {
